7eedf4f0b34b9da03db5562d8d00514a93ad56b0
bugobliterator <siddharthbharatpurohit@gmail.com>
2014-06-09 17:28:15 +0530

    test

--- client/client.cpp
+++ client/client.cpp
@@ -17,7 +17,7 @@ int main(int argc, char *argv[])
 
     if(argc != 2)
     {
-        printf("\n Usage: %s <ip of server> \n",argv[0]);
+        printf("\n Usage : %s <ip of server> \n",argv[0]);
         return 1;
     } 
 
